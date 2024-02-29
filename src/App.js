d619024622adb4acdf314f6651eb6889f4d676a9
pitersizovdev <pitersizov@gmail.com>
2024-02-29 18:17:52 +0300

    0.0.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import styles from './app.module.css'
+import NavPill from './components/widgets/nav/NavPill';
 
 function App() {
   return (
-    <>
-    </>
+    <div className={styles.container}>
+      <NavPill/>
+    </div>
   );
 }
 
